da6b12dbe17cd6e1071271e13687c00419712c48
iyidaclem <iyidaclem@gmail.com>
2022-08-03 23:07:21 +0100

    saved

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
-
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3 alert alert-warning">
 
+            <p class="text-center">Coming soon</p>
+
+        </div>
+    </div>
+</div>
 @endsection
